70e686de31df570d62ae572f26d29a0d4006d53b
SongJianQiang <1511091372@qq.com>
2015-10-30 13:37:18 +0800

    jazz

---
+++ ddd/ddd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ddd
+//
+//  Created by tarena on 15/10/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ddd/ddd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ddd
+//
+//  Created by tarena on 15/10/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
